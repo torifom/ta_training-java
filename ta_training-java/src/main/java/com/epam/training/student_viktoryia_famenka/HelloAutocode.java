04d60d385a63914b69d3ea0eae842b24cec4e3c5
viktoria <vikaok1988@gmail.com>
2023-07-28 20:52:13 +0300

    Hello

---
+++ ta_training-java/src/main/java/com/epam/training/student_viktoryia_famenka/HelloAutocode.java
@@ -0,0 +1,9 @@
+package com.epam.training.student_viktoryia_famenka;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        //Write a program, printing "Hello, Autocode!"
+        System.out.println("Hello, Autocode!");
+
+    }
+}
---
+++ ta_training-java/src/main/java/com/epam/training/student_viktoryia_famenka/MeetAStranger.java
@@ -0,0 +1,12 @@
+package com.epam.training.student_viktoryia_famenka;
+
+import java.util.Scanner;
+    import java.util.Scanner;
+    public class MeetAStranger {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String name = scan.nextLine();
+            System.out.println("Hello, " + name);
+
+        }
+    }
